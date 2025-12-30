d8a6aaf5df36bcde96a5ab614843b133055a35e4
omii88 <suryavanshiom497@gmail.com>
2025-12-30 09:42:29 +0530

    resolved merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,11 @@ import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Projectboard from "./pages/Projectboard";
-<<<<<<< HEAD
 import Sprints from "./pages/Sprints";
 import Boards from "./pages/Boards";
-
-=======
 import Boards from "./pages/Boards"
 import Analytics from "./pages/Analytics";
 import TeamPage from "./pages/TeamPage";
->>>>>>> feature/team-analytics
 
 function App() {
   return (
@@ -30,12 +26,8 @@ function App() {
         <Route path="/project-board" element={<Projectboard />} />
         <Route path="/sprints" element={<Sprints />} />
         <Route path="/boards" element={<Boards />} />
-<<<<<<< HEAD
-=======
         <Route path="/analytics" element={<Analytics />} />
         <Route path="/teampage" element={<TeamPage />} />
-
->>>>>>> feature/team-analytics
       </Routes>
     </>
   );
